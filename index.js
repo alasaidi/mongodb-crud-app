8e1f7bf797249bbb4cb492b929ca74fb8e2ce689
alasaidi <alasaidi55gmail.com>
2024-07-20 20:20:28 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Product = require("./models/product.model.js");
-const productRoute = require("./routes/product.route.js");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import product from "./models/product.model.js";
+
+const port = process.env.PORT;
+const username = process.env.USER_NAME;
+const dbUrl = process.env.DATABASE_URL;
+const apiKey = process.env.API_KEY;
+
 const app = express();
 
-// middleware
+// using express.son middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
-// routes
-app.use("/api/products", productRoute);
-
-
-
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node API Server Updated");
+  res.send("Hello World");
 });
 
+//post product
+app.post("/api", async (req, res) => {
+  try {
+    const products = await product.create(req.body);
 
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get("/api", async (req, res) => {
+  try {
+    const products = await product.find({ quantity: { $gte: 60 } });
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// mongodb cnx setup
 mongoose
-  .connect(
-    "mongodb+srv://haris2iftikhar:GClTzr15XhkjvN6k@backenddb.nrurtot.mongodb.net/Node-API?retryWrites=true&w=majority"
-  )
+  .connect(`mongodb+srv://${username}:${apiKey}@${dbUrl}`)
   .then(() => {
-    console.log("Connected to database!");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    console.log("db connected");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
-  .catch(() => {
-    console.log("Connection failed!");
+  .catch((err) => {
+    console.log(err);
   });
--- models/product.model.js
+++ models/product.model.js
@@ -1,35 +1,15 @@
-const mongoose = require("mongoose");
-
+import mongoose, { model } from "mongoose";
 const ProductSchema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Please enter product name"],
-    },
-
-    quantity: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-
-    price: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-
-    image: {
-      type: String,
-      required: false,
-    },
+    name: { type: String, required: [true, "please entre product name"] },
+    quantity: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    image: { type: String, required: true },
   },
   {
     timestamps: true,
   }
 );
+const product = mongoose.model("Product", ProductSchema);
 
-
-const Product = mongoose.model("Product", ProductSchema);
-
-module.exports = Product;
+export default product;
