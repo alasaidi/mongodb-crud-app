2d255b09fbad968d4445c2fdb5f1e6ce04729417
alasaidi <alasaidi55gmail.com>
2024-07-23 09:40:58 +0200

    add vefify token

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userController from "../controllers/users.controller.js";
+import verifyToken from "../middleware/verifyToken.js";
 const router = express.Router();
 
-router.post("/api", userController.Register);
+router.post("/api", verifyToken, userController.Register);
 router.post("/api/login", userController.login);
+router.get("/api/logout", userController.logout);
 
 export default router;
